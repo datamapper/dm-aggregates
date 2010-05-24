18fcd523d7a625233139ba7a4fd017c79558001b
Dan Kubb <dan.kubb@gmail.com>
2010-05-23 22:55:48 -0700

    Change be_true and be_false to be(true) and be(false) in specs

--- spec/isolated/require_spec.rb
+++ spec/isolated/require_spec.rb
@@ -2,12 +2,12 @@ shared_examples_for "require 'dm-aggregates'" do
 
   %w[ Repository Model Collection Query ].each do |name|
     it "should include the aggregate api in DataMapper::#{name}" do
-      (DataMapper.const_get(name) < DataMapper::Aggregates.const_get(name)).should be_true
+      (DataMapper.const_get(name) < DataMapper::Aggregates.const_get(name)).should be(true)
     end
   end
 
   it "should include the aggregate api into the adapter" do
-    @adapter.respond_to?(:aggregate).should be_true
+    @adapter.respond_to?(:aggregate).should be(true)
   end
 
 end
