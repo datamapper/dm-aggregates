77b474ffff8e97e14f993ff5508ac137df558a2a
Postmodern <postmodern.mod3@gmail.com>
2011-03-01 18:57:25 -0800

    Avoid unnecessary usage of return.

--- lib/dm-aggregates/repository.rb
+++ lib/dm-aggregates/repository.rb
@@ -2,8 +2,11 @@ module DataMapper
   module Aggregates
     module Repository
       def aggregate(query)
-        return [] unless query.valid?
-        adapter.aggregate(query)
+        unless query.valid?
+          []
+        else
+          adapter.aggregate(query)
+        end
       end
     end
   end
