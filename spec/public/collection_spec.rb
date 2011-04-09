796ef9468adcc9e12eb91d6bf1ca416803109592
Dan Kubb <dan.kubb@gmail.com>
2011-04-08 21:37:28 -0700

    Started refactoring specs to use let/subject
    * Still more refactoring needed

--- spec/public/collection_spec.rb
+++ spec/public/collection_spec.rb
@@ -1,141 +1,125 @@
 require 'spec_helper'
 
 describe DataMapper::Collection do
-
-  before :all do
-    @dragons   = Dragon.all
-    @countries = Country.all
-  end
-
   supported_by :sqlite, :mysql, :postgres do
+    let(:dragons)   { Dragon.all  }
+    let(:countries) { Country.all }
 
     it_should_behave_like 'It Has Setup Resources'
     it_should_behave_like 'An Aggregatable Class'
 
     describe 'ignore invalid query' do
-      before :all do
-        @dragons = @dragons.all(:id => [])
-      end
+      let(:dragons) { Dragon.all.all(:id => []) }
 
       [ :size, :count ].each do |method|
         describe "##{method}" do
-          it 'should return 0' do
-            @dragons.send(method).should == 0
-          end
+          subject { dragons.send(method) }
+
+          it { should == 0 }
         end
       end
 
       describe '#min' do
-        it 'should return nil' do
-          @dragons.min(:id).should == nil
-        end
+        subject { dragons.min(:id) }
+
+        it { should be_nil }
       end
 
       describe '#max' do
-        it 'should return nil' do
-          @dragons.max(:id).should == nil
-        end
+        subject { dragons.max(:id) }
+
+        it { should be_nil }
       end
 
       describe '#avg' do
-        it 'should return nil' do
-          @dragons.avg(:id).should == nil
-        end
+        subject { dragons.avg(:id) }
+
+        it { should be_nil }
       end
 
       describe '#sum' do
-        it 'should return nil' do
-          @dragons.sum(:id).should == nil
-        end
+        subject { dragons.sum(:id) }
+
+        it { should be_nil }
       end
 
       describe '#aggregate' do
-        it 'should return nil' do
-          @dragons.aggregate(:id).should == []
-        end
+        subject { dragons.aggregate(:id) }
+
+        it { should == [] }
       end
     end
 
-    describe 'with collections created with Set operations' do
-      before do
-        @collection = @dragons.all(:name => 'George') | @dragons.all(:name => 'Puff')
-      end
+    context 'with collections created with Set operations' do
+      let(:collection) { dragons.all(:name => 'George') | dragons.all(:name => 'Puff') }
 
       describe '#size' do
-        subject { @collection.size }
+        subject { collection.size }
 
         it { should == 2 }
       end
 
       describe '#count' do
-        subject { @collection.count }
+        subject { collection.count }
 
         it { should == 2 }
       end
 
       describe '#min' do
-        subject { @collection.min(:toes_on_claw) }
+        subject { collection.min(:toes_on_claw) }
 
         it { should == 3 }
       end
 
       describe '#max' do
-        subject { @collection.max(:toes_on_claw) }
+        subject { collection.max(:toes_on_claw) }
 
         it { should == 4 }
       end
 
       describe '#avg' do
-        subject { @collection.avg(:toes_on_claw) }
+        subject { collection.avg(:toes_on_claw) }
 
         it { should == 3.5 }
       end
 
       describe '#sum' do
-        subject { @collection.sum(:toes_on_claw) }
+        subject { collection.sum(:toes_on_claw) }
 
         it { should == 7 }
       end
 
       describe '#aggregate' do
-        subject { @collection.aggregate(:all.count, :name.count, :toes_on_claw.min, :toes_on_claw.max, :toes_on_claw.avg, :toes_on_claw.sum)}
+        subject { collection.aggregate(:all.count, :name.count, :toes_on_claw.min, :toes_on_claw.max, :toes_on_claw.avg, :toes_on_claw.sum)}
 
         it { should == [ 2, 2, 3, 4, 3.5, 7 ] }
       end
     end
 
-    describe 'with a collection limited to 1 result' do
+    context 'with a collection limited to 1 result' do
+      let(:dragons) { Dragon.all(:limit => 1) }
 
-      before do
-        @dragons = Dragon.all(:limit => 1)
+      describe '#size' do
+        subject { dragons.size }
+
+        it { should == 1 }
       end
 
-      describe "#size" do
-        it 'should return 1' do
-          @dragons.size.should == 1
-        end
-      end
+      describe '#count' do
+        subject { dragons.count }
 
-      describe "#count" do
-        it 'should return 1' do
-          pending 'TODO: make count apply to the limited collection. Currently limit applies after the count' do
-            @dragons.count.should == 1
-          end
-        end
-      end
-
-    end
-
-    describe 'with the order reversed by the grouping field' do
-      before do
-        @dragons = Dragon.all(:order => [ :birth_at.desc ])
-      end
-
-      it 'should display the results in reverse order' do
-        @dragons.aggregate(:birth_at, :all.count).should == Dragon.aggregate(:birth_at, :all.count).reverse
+        it { pending('TODO: make count apply to the limited collection. Currently limit applies after the count') { should == 1 } }
       end
     end
 
+    context 'with the order reversed by the grouping field' do
+      subject { dragons.aggregate(:birth_at, :all.count) }
+
+      let(:dragons) { Dragon.all(:order => [ :birth_at.desc ]) }
+
+      it 'displays the results in reverse order' do
+        should == Dragon.aggregate(:birth_at, :all.count).reverse
+      end
+    end
   end
-
 end
--- spec/public/model_spec.rb
+++ spec/public/model_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper'
 
 describe DataMapper::Model do
-
-  before :all do
-    @dragons   = Dragon
-    @countries = Country
-  end
-
   supported_by :sqlite, :mysql, :postgres do
+    let(:dragons)   { Dragon  }
+    let(:countries) { Country }
+
     it_should_behave_like 'It Has Setup Resources'
     it_should_behave_like 'An Aggregatable Class'
   end
